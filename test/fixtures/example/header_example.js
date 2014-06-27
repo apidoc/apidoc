e5305935b6bda1450a13895d8b7c6f2150d0c8e0
Peter Rottmann <rottmann@inveris.de>
2014-06-27 13:22:22 +0200

    Add @headerExample.

---
+++ test/fixtures/example/header_example.js
@@ -0,0 +1,10 @@
+/**
+ * @api {get} /header/example/ Header Example
+ * @apiName GetHeaderExample
+ * @apiGroup Header
+ * @apiVersion 0.5.0
+ * @apiDescription Usage of @headerExample.
+ *
+ * @apiHeaderExample An example:
+ *     curl -i http://localhost/header/example/
+ */
