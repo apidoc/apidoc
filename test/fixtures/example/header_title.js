ff02f84f4540557eb8d003c8eec7a6afe5778bfe
Peter Rottmann <rottmann@inveris.de>
2014-06-27 13:16:37 +0200

    Add @headerTitle.

---
+++ test/fixtures/example/header_title.js
@@ -0,0 +1,11 @@
+/**
+ * @api {get} /header/title/ Header Title
+ * @apiName GetHeaderTitle
+ * @apiGroup Header
+ * @apiVersion 0.5.0
+ * @apiDescription Usage of @headerTitle.
+ *
+ * @apiHeaderTitle (MyHeaderGroup) This are the Parameters for MyGroup:
+ * @apiHeader (MyHeaderGroup) authorization The authorization code.
+ * @apiHeader (MyHeaderGroup) {string} text Some text.
+ */
