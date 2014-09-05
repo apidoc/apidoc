d4483aba8b7592e556393edc51b16f72c3152884
Peter Rottmann <rottmann@inveris.de>
2014-09-05 16:56:35 +0200

    Add new example fixture.

---
+++ test/fixtures/example/example.js
@@ -0,0 +1,45 @@
+/**
+ * @api {get} /example/ Example
+ * @apiName GetExample
+ * @apiGroup Example
+ * @apiVersion 0.7.1
+ * @apiDescription Extended usage of @apiExample with different example types.
+ *
+ * @apiExample {PHP} PHP Example (new)
+ * echo 'This is the content. (new)';
+ * @apiExample {JS} JS Example
+ * console.log('This is the content.');
+ *
+ * @apiSuccessExample {PHP} PHP Success Example (new)
+ * echo 'This is the success content. (new)';
+ * @apiSuccessExample {JS} JS Success Example
+ * console.log('This is the success content.');
+ *
+ * @apiErrorExample {PHP} PHP Error Example
+ * echo 'This is the error content.';
+ * @apiErrorExample {JS} JS Error Example
+ * console.log('This is the error content.');
+ */
+
+/**
+ * @api {get} /example/ Example
+ * @apiName GetExample
+ * @apiGroup Example
+ * @apiVersion 0.7.0
+ * @apiDescription Extended usage of @apiExample with different example types.
+ *
+ * @apiExample {PHP} PHP Example
+ * echo 'This is the content.';
+ * @apiExample {JS} JS Example (removed)
+ * console.log('This is the content. (removed)');
+ *
+ * @apiSuccessExample {PHP} PHP Success Example
+ * echo 'This is the success content.';
+ * @apiSuccessExample {JS} JS Success Example
+ * console.log('This is the success content.');
+ *
+ * @apiErrorExample {PHP} PHP Error Example (removed)
+ * echo 'This is the error content. (removed)';
+ * @apiErrorExample {JS} JS Error Example
+ * console.log('This is the error content.');
+ */
