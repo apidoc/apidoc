6e9ad1bb51bf252f03f25e9325a118df817953c4
Peter Rottmann <rottmann@inveris.de>
2014-09-19 18:41:55 +0200

    Start changing apiParam for size.

--- test/fixtures/example/param.js
+++ test/fixtures/example/param.js
@@ -5,25 +5,50 @@
  * @apiVersion 0.1.1
  * @apiDescription Parameters and different Versions: 0.1.1
  *
- * @apiParam {String} param1                 Parameter with type and description.
- * @apiParam {String} param2
- * @apiParam {String} param3="Default Value" Parameter with type, description and default value.
- * @apiParam {String} param4="Default Value"
+ * @apiParam param1                 Parameter and description.
+ * @apiParam param2
+ * @apiParam param3="Default Value" Parameter, default value and description.
+ * @apiParam param4="Default Value"
  *
- * @apiParam param5                 Basic Parameter with description.
- * @apiParam param6
- * @apiParam param7="Default Value" Basic Parameter with description and default value.
- * @apiParam param8="Default Value"
+ * @apiParam [param5]                 Optional parameter and description.
+ * @apiParam [param6]
+ * @apiParam [param7="Default Value"] Optional parameter, default value and description.
+ * @apiParam [param8="Default Value"]
  *
- * @apiParam [param9]                  Optional basic Parameter with description.
- * @apiParam [param10]
- * @apiParam [param11="Default Value"] Optional basic Parameter with description and default value.
- * @apiParam [param12="Default Value"]
+ * @apiParam param9 (4711)                 Parameter, allowed value and description.
+ * @apiParam param10 (4711)
+ * @apiParam param11="Default Value" (4711) Parameter, default value, allowed value and description.
+ * @apiParam param12="Default Value" (4711)
  *
- * @apiParam {String} [param13]                 Optional Parameter with type and description.
- * @apiParam {String} [param14]
- * @apiParam {String} [param15="Default Value"] Optional Parameter with type, description and default value.
- * @apiParam {String} [param16="Default Value"]
+ * @apiParam {String} param13                  Type, parameter and description.
+ * @apiParam {String} param14
+ * @apiParam {String} param15="Default Value" Type, parameter and default value.
+ * @apiParam {String} param16="Default Value"
+ *
+ * @apiParam {String} [param17]                 Type, optional parameter and description.
+ * @apiParam {String} [param18]
+ * @apiParam {String} [param19="Default Value"] Type, optional parameter, default value and description.
+ * @apiParam {String} [param20="Default Value"]
+ *
+ * @apiParam {String} param21 (4711)                 Type, parameter, allowed value and description.
+ * @apiParam {String} param22 (4711)
+ * @apiParam {String} param23="Default Value" (4711) Type, parameter, default value, allowed value and description.
+ * @apiParam {String} param24="Default Value" (4711)
+ *
+ * @apiParam {String} [param25] (4711)                 Type, optional parameter, allowed value and description.
+ * @apiParam {String} [param26] (4711)
+ * @apiParam {String} [param27="Default Value"] (4711) Type, optional parameter, default value, allowed value and description.
+ * @apiParam {String} [param28="Default Value"] (4711)
+ *
+ * @apiParam {String} [param29] ("allowed1", "allowed2")                 Type, optional parameter, allowed strings and description.
+ * @apiParam {String} [param30] ("allowed1", "allowed2")
+ * @apiParam {String} [param31="Default Value"] ("allowed1", "allowed2") Type, optional parameter, default value, allowed strings and description.
+ * @apiParam {String} [param32="Default Value"] ("allowed1", "allowed2")
+ *
+ * @apiParam {String} param33="Default Value" {4,8} Type, parameter, default value, size and description.
+ * @apiParam {Number} param34=1 {1-3}               Type, parameter, default value, size and description.
+ * @apiParam {Number} param35=2                     Type, parameter, default value and description.
+ * @apiParam {Number} param36=2
  */
 
 /**
