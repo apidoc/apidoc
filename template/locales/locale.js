b0f49e8f1996beaa9517478e9f732aa19c71dd66
Peter <rottmann@inveris.de>
2015-05-08 11:20:42 +0200

    Add Brazilian translation.

--- template/locales/locale.js
+++ template/locales/locale.js
@@ -3,27 +3,30 @@ define([
     './locales/fr.js',
     './locales/nl.js',
     './locales/pl.js',
+    './locales/pt_br.js',
     './locales/ru.js',
     './locales/zh.js'
 ], function() {
+    var langId = (navigator.language || navigator.userLanguage).toLowerCase();
+    var language = langId.substr(0, 2);
     var locales = {};
-    for(index in arguments) {
-        for(property in arguments[index]) {
-            locales[property] = arguments[index][property];
-        }
-    }
 
-    var language = ((navigator.language) ? navigator.language : navigator.userLanguage).substr(0, 2).toLowerCase();
-    if( ! locales['en'])
+    for (index in arguments) {
+        for (property in arguments[index])
+            locales[property] = arguments[index][property];
+    }
+    if ( ! locales['en'])
         locales['en'] = {};
-    if( ! locales[language])
+
+    if ( ! locales[langId] && ! locales[language])
         language = 'en';
 
-    var locale = locales[language];
+    var locale = (locales[langId] ? locales[langId] : locales[language]);
 
     var __ = function(text) {
         var index = locale[text];
-        if(index === undefined) return text;
+        if (index === undefined)
+            return text;
         return index;
     };
 
