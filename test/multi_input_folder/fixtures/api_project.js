3baedf77f7d9c62abd88c7c7d87d973784f64507
Peter <rottmann@inveris.de>
2015-05-08 12:50:10 +0200

    Add multiple input locations.

---
+++ test/multi_input_folder/fixtures/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "Multiple Input Folder Test",
+  "version": "1.0.0",
+  "description": "Example of feeding apidoc from multiple input folder locations",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-05-08T10:26:38.294Z",
+    "url": "http://apidocjs.com",
+    "version": "0.12.3"
+  }
+});
