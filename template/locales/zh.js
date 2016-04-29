99a1ebb50452b2529d1f091a703ca4c502e36ae6
Steven Chen <neko2891@gmail.com>
2016-04-29 18:09:35 +0800

    [fix] Allowed values not work，translation localization

--- template/locales/zh.js
+++ template/locales/zh.js
@@ -1,6 +1,6 @@
 define({
     zh: {
-        'Allowed values​​:'             : '允許值:',
+        'Allowed values:'             : '允許值:',
         'Compare all with predecessor': '預先比較所有',
         'compare changes to:'         : '比較變更:',
         'compared to'                 : '對比',
@@ -11,9 +11,9 @@ define({
         'Generated with'              : '生成工具',
         'Name'                        : '名稱',
         'No response values​​.'         : '無對應資料.',
-        'optional'                    : '選項',
+        'optional'                    : '非必填',
         'Parameter'                   : '參數',
-        'Permission:'                 : '允許:',
+        'Permission:'                 : '權限:',
         'Response'                    : '回應',
         'Send'                        : '發送',
         'Send a Sample Request'       : '發送試用需求',
