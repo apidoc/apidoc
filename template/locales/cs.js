d43a2f6195fd1c2bcc1f315b6aa64691a4c6074d
Ondřej Misák <email@ondrejmisak.cz>
2018-06-19 21:20:25 +0200

    Add Czech translation.

---
+++ template/locales/cs.js
@@ -0,0 +1,25 @@
+define({
+    cs: {
+        'Allowed values:'             : 'Povolené hodnoty:',
+        'Compare all with predecessor': 'Porovnejte s předchozími verzemi',
+        'compare changes to:'         : 'porovnat změny s:',
+        'compared to'                 : 'porovnat s',
+        'Default value:'              : 'Výchozí hodnota:',
+        'Description'                 : 'Popis',
+        'Field'                       : 'Pole',
+        'General'                     : 'Obecné',
+        'Generated with'              : 'Vygenerováno pomocí',
+        'Name'                        : 'Název',
+        'No response values.'         : 'Nevráceny žádné hodnoty.',
+        'optional'                    : 'volitelné',
+        'Parameter'                   : 'Parametr',
+        'Permission:'                 : 'Oprávnění:',
+        'Response'                    : 'Odpověď',
+        'Send'                        : 'Odeslat',
+        'Send a Sample Request'       : 'Odeslat testovací požadavek',
+        'show up to version:'         : 'zobrazit po verzi:',
+        'Size range:'                 : 'Rozsah velikosti:',
+        'Type'                        : 'Typ',
+        'url'                         : 'url'
+    }
+});
