cad2ba47554dbaaa76bca6100519b65b4e2a4a11
Neel Bhanushali <neal.bhanushali@gmail.com>
2020-01-15 02:59:37 +0530

    sent provided trueValue or falseValue on the basis if the checkbox is checked

--- template/utils/send_sample_request.js
+++ template/utils/send_sample_request.js
@@ -1,13 +1,14 @@
 define([
     'jquery',
     'lodash',
-    './utils/send_sample_request_utils'
-], function($, _, utils) {
+    './utils/send_sample_request_utils',
+    './api_data.js'
+], function ($, _, utils, data) {
 
-    var initDynamic = function() {
+    var initDynamic = function () {
         // Button send
         $(".sample-request-send").off("click");
-        $(".sample-request-send").on("click", function(e) {
+        $(".sample-request-send").on("click", function (e) {
             e.preventDefault();
             var $root = $(this).parents("article");
             var group = $root.data("group");
@@ -18,7 +19,7 @@ define([
 
         // Button clear
         $(".sample-request-clear").off("click");
-        $(".sample-request-clear").on("click", function(e) {
+        $(".sample-request-clear").on("click", function (e) {
             e.preventDefault();
             var $root = $(this).parents("article");
             var group = $root.data("group");
@@ -28,21 +29,23 @@ define([
         });
     }; // initDynamic
 
-    function sendSampleRequest(group, name, version, type)
-    {
+    function sendSampleRequest(group, name, version, type) {
+        var apiData = data.api.find(function (d) {
+            return d.name == name && d.version == version;
+        })
         var $root = $('article[data-group="' + group + '"][data-name="' + name + '"][data-version="' + version + '"]');
 
         // Optional header
         var header = {};
-        $root.find(".sample-request-header:checked").each(function(i, element) {
+        $root.find(".sample-request-header:checked").each(function (i, element) {
             var group = $(element).data("sample-request-header-group-id");
-            $root.find("[data-sample-request-header-group=\"" + group + "\"]").each(function(i, element) {
+            $root.find("[data-sample-request-header-group=\"" + group + "\"]").each(function (i, element) {
                 var key = $(element).data("sample-request-header-name");
                 var value = element.value;
                 if (typeof element.optional === 'undefined') {
-                  element.optional = true;
+                    element.optional = true;
                 }
-                if ( ! element.optional && element.defaultValue !== '') {
+                if (!element.optional && element.defaultValue !== '') {
                     value = element.defaultValue;
                 }
                 header[key] = value;
@@ -56,32 +59,38 @@ define([
         var bodyFormData = {};
         var bodyFormDataType = {};
         var bodyJson = '';
-        $root.find(".sample-request-param:checked").each(function(i, element) {
+        $root.find(".sample-request-param:checked").each(function (i, element) {
             var group = $(element).data("sample-request-param-group-id");
             var contentType = $(element).nextAll('.sample-header-content-type-switch').first().val();
-            if (contentType == "body-json"){
-                $root.find("[data-sample-request-body-group=\"" + group + "\"]").not(function(){
+            if (contentType == "body-json") {
+                $root.find("[data-sample-request-body-group=\"" + group + "\"]").not(function () {
                     return $(this).val() == "" && $(this).is("[data-sample-request-param-optional='true']");
-                }).each(function(i, element) {
-                    if (isJson(element.value)){
+                }).each(function (i, element) {
+                    if (isJson(element.value)) {
                         header['Content-Type'] = 'application/json';
                         bodyJson = element.value;
                     }
                 });
-            }else {
-                $root.find("[data-sample-request-param-group=\"" + group + "\"]").not(function(){
+            } else {
+                $root.find("[data-sample-request-param-group=\"" + group + "\"]").not(function () {
                     return $(this).val() == "" && $(this).is("[data-sample-request-param-optional='true']");
-                }).each(function(i, element) {
+                }).each(function (i, element) {
                     var key = $(element).data("sample-request-param-name");
                     var value = element.value;
-                    if ( ! element.optional && element.defaultValue !== '') {
+                    if (!element.optional && element.defaultValue !== '') {
                         value = element.defaultValue;
                     }
-                    if (contentType == "body-form-data"){
+                    var paramData = apiData.parameter.fields.Parameter[i]
+                    if (paramData.type == 'Boolean') {
+                        var trueValue = paramData.allowedValues[0]
+                        var falseValue = paramData.allowedValues[1]
+                        value = element.checked ? trueValue : falseValue
+                    }
+                    if (contentType == "body-form-data") {
                         header['Content-Type'] = 'multipart/form-data'
                         bodyFormData[key] = value;
                         bodyFormDataType[key] = $(element).next().text();
-                    }else {
+                    } else {
                         param[key] = value;
                         paramType[key] = $(element).next().text();
                     }
@@ -93,7 +102,7 @@ define([
         var url = $root.find(".sample-request-url").val();
 
         //Convert {param} form to :param
-        url = url.replace(/{/,':').replace(/}/,'');
+        url = url.replace(/{/, ':').replace(/}/, '');
 
         // Insert url parameter
         var pattern = pathToRegexp(url, null);
@@ -112,10 +121,10 @@ define([
         param = utils.handleNestedAndParsingFields(param, paramType);
 
         //add url search parameter
-        if (header['Content-Type'] == 'application/json' ){
+        if (header['Content-Type'] == 'application/json') {
             url = url + encodeSearchParams(param);
             param = bodyJson;
-        }else if (header['Content-Type'] == 'multipart/form-data'){
+        } else if (header['Content-Type'] == 'multipart/form-data') {
             url = url + encodeSearchParams(param);
             param = bodyFormData;
         }
@@ -126,12 +135,12 @@ define([
 
         // send AJAX request, catch success or error callback
         var ajaxRequest = {
-            url        : url,
-            headers    : header,
-            data       : param,
-            type       : type.toUpperCase(),
-            success    : displaySuccess,
-            error      : displayError
+            url: url,
+            headers: header,
+            data: param,
+            type: type.toUpperCase(),
+            success: displaySuccess,
+            error: displayError
         };
 
         $.ajax(ajaxRequest);
@@ -163,7 +172,7 @@ define([
                 message += "\n" + jsonResponse;
 
             // flicker on previous error to make clear that there is a new response
-            if($root.find(".sample-request-response").is(":visible"))
+            if ($root.find(".sample-request-response").is(":visible"))
                 $root.find(".sample-request-response").fadeTo(1, 0.1);
 
             $root.find(".sample-request-response").fadeTo(250, 1);
@@ -172,8 +181,7 @@ define([
         };
     }
 
-    function clearSampleRequest(group, name, version)
-    {
+    function clearSampleRequest(group, name, version) {
         var $root = $('article[data-group="' + group + '"][data-name="' + name + '"][data-version="' + version + '"]');
 
         // hide sample response
@@ -181,7 +189,7 @@ define([
         $root.find(".sample-request-response").hide();
 
         // reset value of parameters
-        $root.find(".sample-request-param").each(function(i, element) {
+        $root.find(".sample-request-param").each(function (i, element) {
             element.value = "";
         });
 
@@ -192,8 +200,7 @@ define([
         refreshScrollSpy();
     }
 
-    function refreshScrollSpy()
-    {
+    function refreshScrollSpy() {
         $('[data-spy="scroll"]').each(function () {
             $(this).scrollspy("refresh");
         });
@@ -212,13 +219,13 @@ define([
     function isJson(str) {
         if (typeof str == 'string') {
             try {
-                var obj=JSON.parse(str);
-                if(typeof obj == 'object' && obj ){
+                var obj = JSON.parse(str);
+                if (typeof obj == 'object' && obj) {
                     return true;
-                }else{
+                } else {
                     return false;
                 }
-            } catch(e) {
+            } catch (e) {
                 return false;
             }
         }
