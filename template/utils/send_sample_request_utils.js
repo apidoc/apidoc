1637a2f73f37ae2825353607bfd3c10b2c431897
rafaelgssa <rafael.gssa@pm.me>
2020-04-25 20:24:39 -0300

    Add comment

--- template/utils/send_sample_request_utils.js
+++ template/utils/send_sample_request_utils.js
@@ -47,6 +47,7 @@ define([], function () {
         return result;
     }
 
+    // Converts path params in the {param} format to the accepted :param format, used before inserting the URL params.
     function convertPathParams(url) {
         return url.replace(/{(.+?)}/g, ':$1');
     }
