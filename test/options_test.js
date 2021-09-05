21ca9dc617477e981178675bcd40f5f2164c0c19
Nicolas CARPi <nico-git@deltablot.email>
2021-09-05 23:09:49 +0200

    introduce the options module and its tests

---
+++ test/options_test.js
@@ -0,0 +1,42 @@
+/**
+ * Test the options module
+ */
+const assert = require('assert');
+const path = require('path');
+
+const optionsProcessor = require('../lib/options');
+
+describe('test options module', function () {
+  it('should provide default options when no options are given', function (done) {
+    const processedOptions = optionsProcessor.process({});
+    assert.deepEqual(processedOptions, optionsProcessor.defaultOptions);
+    done();
+  });
+
+  it('should provide correct options when options are given', function (done) {
+    const options = {
+      src: ['some/path'],
+      dest: 'output/path',
+      debug: true,
+    };
+    const processedOptions = optionsProcessor.process(options);
+    assert.strictEqual(processedOptions.src, options.src);
+    assert.strictEqual(processedOptions.dest, options.dest + path.sep);
+    assert.strictEqual(processedOptions.debug, options.debug);
+    done();
+  });
+
+  it('should read input/output paths from config file', function (done) {
+    const options = {
+      config: 'example/apidoc.json',
+    };
+    // read the apidoc.json file
+    const apidocJson = require('../example/apidoc.json');
+
+    const processedOptions = optionsProcessor.process(options);
+
+    assert.deepEqual(processedOptions.src, apidocJson.input.map(p => path.resolve(p) + path.sep));
+    assert.strictEqual(processedOptions.dest, apidocJson.output + path.sep);
+    done();
+  });
+});
