571c5a8d8cde9c1b267622857d2cef8514c9b21c
Emmanuel Saracco <emmanuel@esaracco.fr>
2021-10-13 14:12:27 +0200

    fix DiffMatchPatch for test mode

--- template/src/diff_match_patch.mjs
+++ template/src/diff_match_patch.mjs
@@ -1,6 +1,11 @@
 import _DiffMatchPatch from 'diff-match-patch';
 
 export default class DiffMatchPatch extends _DiffMatchPatch {
+  constructor (testMode) {
+    super();
+    this.testMode = testMode;
+  }
+
   diffMain (text1, text2, optChecklines, optDeadline) {
     return super.diff_main(this._stripHtml(text1), this._stripHtml(text2), optChecklines, optDeadline);
   }
@@ -36,6 +41,8 @@ export default class DiffMatchPatch extends _DiffMatchPatch {
   }
 
   _stripHtml (html) {
+    // no document object with CLI when running tests
+    if (this.testMode) { return html; }
     const div = document.createElement('div');
     div.innerHTML = html;
     return div.textContent || div.innerText || '';
