098d2130c4637965914ffb3070f53a63fd64e68f
Emmanuel Saracco <emmanuel@esaracco.fr>
2021-10-15 15:14:45 +0200

    fix adding parameters to the query string (#1036)
    fix #1035

--- test/sampreq_url_processor_test.mjs
+++ test/sampreq_url_processor_test.mjs
@@ -45,12 +45,21 @@ describe('test sample request url processor', () => {
     done();
   });
 
-  it('should hydrate correctly an url with standard parameter', done => {
+  it('case 1: should hydrate correctly an url with standard parameter', done => {
     const dryUrl = 'https://api.example.org/user';
     const parameters = {};
     parameters['view'] = 'aerial';
     const url = UrlProc.hydrate(dryUrl, parameters);
     assert.strictEqual(url, 'https://api.example.org/user?view=aerial');
+    done();
+  });
+
+  it('case 2: should hydrate correctly an url with standard parameter', done => {
+    const dryUrl = 'https://api.example.org/user?dev=1';
+    const parameters = {};
+    parameters['view'] = 'aerial';
+    const url = UrlProc.hydrate(dryUrl, parameters);
+    assert.strictEqual(url, 'https://api.example.org/user?dev=1&view=aerial');
     done();
   });
 
