f66f7c1fe6f2f76a02c2a9e08c357552c73fe42e
BrokenSourceCode <59090546+BrokenSourceCode@users.noreply.github.com>
2022-03-30 00:03:13 +0200

    Fix apidoc/apidoc#1194 (ESLint)

--- template/src/sampreq_url_processor.mjs
+++ template/src/sampreq_url_processor.mjs
@@ -12,12 +12,11 @@ export default class UrlProcessor {
   // Replace parameters from url (:id) by the parameters from input values
   hydrate (url, queryParameters) {
     // The dummy URL base is only used for parses of relative URLs in Node.js.
-    const parsedUrl = new URL(url, (typeof window === 'undefined') ? 'https://dummy.base' : window.location.origin);
-	  const queryParametersChangedInPathname = {};
+    const parsedUrl = new URL(url, typeof window === 'undefined' ? 'https://dummy.base' : window.location.origin);
+    const queryParametersChangedInPathname = {};
 
     // For API parameters in the URL parts delimited by `/` (e.g. `/:foo/:bar`).
-    parsedUrl.pathname.split('/').forEach((pathnamePart, i) =>
-    {
+    parsedUrl.pathname.split('/').forEach((pathnamePart, i) => {
       if (pathnamePart.charAt(0) === ':') {
         const realPathnamePart = pathnamePart.slice(1);
 
@@ -29,11 +28,10 @@ export default class UrlProcessor {
     });
 
     // For API parameters in the URL query string (e.g. `?foo=:foo&bar=:bar`).
-    for (const key in queryParameters)
-    {
+    for (const key in queryParameters) {
       if (
-        typeof queryParametersChangedInPathname[key] === 'undefined' // Avoid adding query parameter if it has already been changed in pathname.
-        || parsedUrl.searchParams.has(key)
+        typeof queryParametersChangedInPathname[key] === 'undefined' || // Avoid adding query parameter if it has already been changed in pathname.
+        parsedUrl.searchParams.has(key)
       ) {
         parsedUrl.searchParams.set(key, queryParameters[key]);
       }
