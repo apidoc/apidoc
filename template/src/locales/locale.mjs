cb15b6027eff27a5549371eb58922ed687a2234d
Nicolas CARPi <nico-git@deltablot.email>
2022-07-16 14:44:25 +0200

    allow several keys for pt or cn, give better error message if locale is
    incorrect. fix #1248

--- template/src/locales/locale.mjs
+++ template/src/locales/locale.mjs
@@ -34,11 +34,14 @@ const locales = {
   nl: nl,
   pl: pl,
   pt: ptBr,
+  pt_br: ptBr,
   ro: ro,
   ru: ru,
   tr: tr,
   vi: vi,
+  // for chinese, allow zh and zh_cn
   zh: zhCn,
+  zh_cn: zhCn,
 };
 
 // e.g. en fr pl
@@ -53,5 +56,8 @@ export function __ (text) {
 }
 
 export function setLanguage (language) {
+  if (!Object.prototype.hasOwnProperty.call(locales, language)) {
+    throw new Error(`Invalid value for language setting! Available values are ${Object.keys(locales).join(',')}`);
+  }
   locale = locales[language];
 }
