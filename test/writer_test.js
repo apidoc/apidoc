ddf6de4c6a68ce1551b56ceb8893c97401d81251
MichalS <m.siemienowicz.m@gmail.com>
2022-11-11 12:05:56 +0100

    bugfix: improve the assignment of conditional variable values (#1296)
    * bugfix: improve the assignment of conditional variable values
    
    * fix: if debug flag is not used set webpack devtool to false
    
    * test: post-review fixes

--- test/writer_test.js
+++ test/writer_test.js
@@ -13,8 +13,6 @@
  * Test the writer module
  */
 const assert = require('assert');
-const path = require('path');
-
 const logger = require('./silentlogger');
 const Writer = require('../lib/writer');
 
@@ -29,5 +27,25 @@ describe('test writer module', function () {
 
     const writer = new Writer({}, app);
     return writer.write();
+  });
+
+  it('should work in dry run mode with debug option', async function () {
+    const app = {
+      options: {
+        dryRun: true,
+        debug: true,
+      },
+      log: logger,
+    };
+    const writer = new Writer({}, app);
+    return writer.write();
+  });
+
+  it('getIndexContent() should throw an error if project (apiProject) is undefined', async function () {
+    const app = {
+      log: logger,
+    };
+    const writer = new Writer({}, app);
+    assert.throws(writer.getIndexContent, Error);
   });
 });
